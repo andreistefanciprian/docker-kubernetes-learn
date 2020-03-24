869dc4367225c6b06a50bc20edf8750024cf82df
“Stefan <andreistefanciprian@gmail.com>
2020-03-24 10:49:14 +0000

    update

--- docker/dockerapp/app/app.py
+++ docker/dockerapp/app/app.py
@@ -1,9 +1,28 @@
 from flask import Flask
+import math
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+
+@app.route('/cpu_load')
+
+def cpu_load():
+    result = []
+    def cpu_load_gen():
+        # for x in range(n):
+        #     return (math.sqrt(0.000001))
+
+        while True:
+            # for x in range(n):
+            result.append(math.sqrt(0.000001))
+        return result
+
+    return cpu_load_gen()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
